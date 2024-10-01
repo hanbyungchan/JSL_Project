389952ef8de0a663ec8e7fd4bd1b2277617ab5b9
admin <admin@admin-pc010>
2024-10-01 18:51:40 +0900

    han11

---
+++ prj_YJGames/src/main/java/dao/UserDao.java
@@ -0,0 +1,23 @@
+package dao;
+
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import common.CommonTemplate;
+
+public class UserDao {
+	JdbcTemplate temp = CommonTemplate.getTemplate();
+	
+	//등록
+	
+	
+	//로그인
+	
+	
+	//내 정보
+	
+	
+	//수정
+	
+	
+	//탈퇴
+}
---
+++ prj_YJGames/src/main/java/dto/UserDto.java
@@ -0,0 +1,77 @@
+package dto;
+
+public class UserDto {
+	String u_id, u_name, u_password, u_birth, u_game_token, u_gender, u_email_1, u_email_2;
+
+	public UserDto() {
+		super();
+	}
+
+	public String getU_id() {
+		return u_id;
+	}
+
+	public void setU_id(String u_id) {
+		this.u_id = u_id;
+	}
+
+	public String getU_name() {
+		return u_name;
+	}
+
+	public void setU_name(String u_name) {
+		this.u_name = u_name;
+	}
+
+	public String getU_password() {
+		return u_password;
+	}
+
+	public void setU_password(String u_password) {
+		this.u_password = u_password;
+	}
+
+	public String getU_birth() {
+		return u_birth;
+	}
+
+	public void setU_birth(String u_birth) {
+		this.u_birth = u_birth;
+	}
+
+	public String getU_game_token() {
+		return u_game_token;
+	}
+
+	public void setU_game_token(String u_game_token) {
+		this.u_game_token = u_game_token;
+	}
+
+	public String getU_gender() {
+		return u_gender;
+	}
+
+	public void setU_gender(String u_gender) {
+		this.u_gender = u_gender;
+	}
+
+	public String getU_email_1() {
+		return u_email_1;
+	}
+
+	public void setU_email_1(String u_email_1) {
+		this.u_email_1 = u_email_1;
+	}
+
+	public String getU_email_2() {
+		return u_email_2;
+	}
+
+	public void setU_email_2(String u_email_2) {
+		this.u_email_2 = u_email_2;
+	}
+	
+	
+	
+	
+}
