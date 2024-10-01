eb57cf74490dcfa3cbeb265ae98f555824341e18
JSLHRD <JSLHRD@JSL-I7-006>
2024-10-01 16:36:47 +0900

    123

---
+++ prj_YJGames/src/main/java/dto/HomeDto.java
@@ -0,0 +1,38 @@
+package dto;
+
+public class HomeDto {
+	String s_list_no,g_name,s_sale,s_attach;
+	int g_price;
+	public HomeDto() {}
+	public String getS_list_no() {
+		return s_list_no;
+	}
+	public void setS_list_no(String s_list_no) {
+		this.s_list_no = s_list_no;
+	}
+	public String getG_name() {
+		return g_name;
+	}
+	public void setG_name(String g_name) {
+		this.g_name = g_name;
+	}
+	public String getS_sale() {
+		return s_sale;
+	}
+	public void setS_sale(String s_sale) {
+		this.s_sale = s_sale;
+	}
+	public String getS_attach() {
+		return s_attach;
+	}
+	public void setS_attach(String s_attach) {
+		this.s_attach = s_attach;
+	}
+	public int getG_price() {
+		return g_price;
+	}
+	public void setG_price(int g_price) {
+		this.g_price = g_price;
+	}
+	
+}
