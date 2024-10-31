9f01a268bc4801f5a80c1a07d3d670faac5da631
DESKTOP-IG7DFR1\JSL <ume143@naver.com>
2024-10-31 18:59:21 +0900

    241031_2

---
+++ prj_YJGames_241010/src/main/java/command/rank/Rank.java
@@ -0,0 +1,16 @@
+package command.rank;
+
+import javax.servlet.http.HttpServletRequest;
+
+import common.CommonExecute;
+
+public class Rank implements CommonExecute {
+
+	@Override
+	public void execute(HttpServletRequest request) {
+		
+		
+		
+	}
+
+}
--- prj_YJGames_241010/src/main/java/dao/RankDao.java
+++ prj_YJGames_241010/src/main/java/dao/RankDao.java
@@ -7,7 +7,10 @@ import common.CommonTemplate;
 public class RankDao {
 	JdbcTemplate temp = CommonTemplate.getTemplate();
 	
-	
+	//랭킹 테이블의 데이터들 올라가는 hit 함수 만들기.
+	//게임 등록시 랭킹 테이블에도 자동적으로 등록
+	//페이지 자체는 DB대로 랭킹 테이블 나열만 하면 됨.
+	//컨트롤러 구분을 할까 말까 고민중... 일단 다 만들고 나서 내가 작업하던가 하자 이건.
 	
 	
 	
